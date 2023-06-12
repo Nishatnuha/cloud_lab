7f83cf563643ad160789cbec6d0a17376bbae44f
Nishatnuha <48322328+Nishatnuha@users.noreply.github.com>
2023-06-12 18:22:08 +0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import flask
-from flask import jsonify
-
-app = flask.Flask(__name__)
-
-@app.route('/generate_even_numbers/<int:n>', methods=['GET'])
-def generate_even_numbers(n):
-    if n < 0:
-       
